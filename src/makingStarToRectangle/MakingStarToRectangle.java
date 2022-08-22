f253d38d2f47ad0f9868d1f73d7b28db0a71caa6
namtndus <rnrmf0721@gmail.com>
2022-08-22 11:31:55 +0900

    [프로그래머스] 직사각형 별찍기 성공

--- src/makingStarToRectangle/MakingStarToRectangle.java
+++ src/makingStarToRectangle/MakingStarToRectangle.java
@@ -1,7 +1,21 @@
 package makingStarToRectangle;
 
+import java.util.Scanner;
+
 public class MakingStarToRectangle {
     public static void main(String[] args) {
-        System.out.println("성공했다.");
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();  // 가로의 길이
+        int b = sc.nextInt();  // 세로의 길이
+
+        for(int i = 0; i<b; i++){
+            for(int j = 0; j<a; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
     }
 }
